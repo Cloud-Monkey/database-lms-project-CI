b64a934d3ea91282025c8e371133a6f0aad2a947
Michael Brown <ironmonkeydaveb@yahoo.co.uk>
2024-08-28 10:49:59 +0000

    add fetch variable for results multiple

--- sql-psycopg2.py
+++ sql-psycopg2.py
@@ -5,3 +5,7 @@ connection = psycopg2.connect(database="chinook")
 
 # build a cursor object of the database
 cursor = connection.cursor()
+
+# fetch results (multiple)
+results = cursor.fetchall()
+
