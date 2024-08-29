ac84645dbdc485629b47abc1335e9d7aa7725a24
Michael Brown <ironmonkeydaveb@yahoo.co.uk>
2024-08-29 10:14:13 +0000

    add variable for track table in database

--- sql-expression.py
+++ sql-expression.py
@@ -22,5 +22,19 @@ album_table = Table (
     Column("ArtistId", Integer, ForeignKey("artist_table.ArtistId"))
 )
 
+# create variable for "Track" table
+track_table = Table (
+    "Track", meta,
+    Column("TrackID", Integer, primary_key=True),
+    Column("Name", String),
+    Column("AlbumId", Integer, ForeignKey("album_table.AlbumId")),
+    Column("MediaTypeId", Integer, primary_key=False),
+    Column("GenreId", Integer, primary_key=False),
+    Column("Composer", String),
+    Column("Milliseconds", Integer),
+    Column("Bytes", Integer),
+    Column("UnitPrice", Float)
+)
+
 # making the connection
 with db.connect() as connection:
