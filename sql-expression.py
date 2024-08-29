574e7ffc72b9559a655f60eccbf34bdb2a9d59f0
Michael Brown <ironmonkeydaveb@yahoo.co.uk>
2024-08-29 10:01:21 +0000

    add variable for album table

--- sql-expression.py
+++ sql-expression.py
@@ -7,9 +7,18 @@ db = create_engine("postgresql:///chinook")
 
 meta = MetaData(db)
 
-# create variable for artist table
+# create variable for "Artist" table
 artist_table = Table (
     "Artist", meta,
+    Column("ArtistId", Integer, primary_key=True),
+    Column("Name", String)
+)
+
+# create variable for "Album" table
+album_table = Table (
+    "Name", meta,
+    Column("AlbumId", Integer, primary_key=True),
+    Column("Title", String)
 )
 
 # making the connection
