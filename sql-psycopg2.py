99ce31a831ed113426a3a4e44dfba66ef5723c0e
Michael Brown <ironmonkeydaveb@yahoo.co.uk>
2024-08-29 08:32:24 +0000

    add query to select artist id of 51 from the artist table in the database

--- sql-psycopg2.py
+++ sql-psycopg2.py
@@ -6,17 +6,23 @@ connection = psycopg2.connect(database="chinook")
 # build a cursor object of the database
 cursor = connection.cursor()
 
-# query 1 - select all records from the "artist" table
-cursor.execute('SELECT * FROM "Artist"')
+# query 1 - select all records from the "Artist" table
+# cursor.execute('SELECT * FROM "Artist"')
 
-# query 2 - select "name" records from the "artist" table
-cursor.execute('SELECT "Name" FROM "Artist"')
+# query 2 - select "name" records from the "Artist" table
+# cursor.execute('SELECT "Name" FROM "Artist"')
+
+# query 3 - select "Queen" "Name" from the "Artist" table
+# cursor.execute('SELECT * FROM "Artist" WHERE "Name" = %s ', ["Queen"])
+
+# query 4 - select "ArtistId" 51 from the "Artist" table
+cursor.execute('SELECT * FROM "Artist" WHERE "ArtistId" = %s ', [51])
 
 # fetch results (multiple)
-results = cursor.fetchall()
+# results = cursor.fetchall()
 
 # fetch results (single)
-# results = cursor.fetchone()
+results = cursor.fetchone()
 
 # close the connection
 connection.close()
