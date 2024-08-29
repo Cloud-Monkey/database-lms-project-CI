5e7b4f1cbc0095744d172ca61a709ce5dd2dce63
Michael Brown <ironmonkeydaveb@yahoo.co.uk>
2024-08-29 09:49:28 +0000

    add variable fo local host instructions for database

--- sql-expression.py
+++ sql-expression.py
@@ -2,3 +2,5 @@ from sqlalchemy import (
     create_engine, Table, Column, Float, ForeignKey, Integer, String, MetaData
 )
 
+# executing the instructions for our localhost "chinook" db
+db = create_engine("postgresql:///chinook")
