3724f6c75200d1622761b2cd03e77df41592dd69
Michael Brown <ironmonkeydaveb@yahoo.co.uk>
2024-08-30 13:09:45 +0000

    add a class based model for the track table

--- sql-orm.py
+++ sql-orm.py
@@ -22,6 +22,18 @@ class Album(base):
     ArtistId = Column(Integer, ForeignKey("Artist.ArtistId"))
 
 # create a class based model for the track table
+class Track(base):
+    __tablename__ = "Track"
+    TrackId = Column(Integer, primary_key=True)
+    Name = Column(String)
+    AlbumId = Column(Integer, ForeignKey("Album.AlbumId"))
+    MediaTypeId = Column(Integer, primary_key=False)
+    GenreId = Column(Integer, primary_key=False)
+    Composer = Column(String)
+    Milliseconds = Column(Integer, primary_key=False)
+    Bytes = Column(Integer, primary_key=False)
+    UnitPrice = Column(Float)
+
 
 
 # instead of connecting to the database directly, we will ask for a session
