a3726114c959e0162f278aa6c68f443263b9f17b
Michael Brown <ironmonkeydaveb@yahoo.co.uk>
2024-08-30 11:53:41 +0000

    add db variable to execute instructions from the database

---
+++ sql-orm.py
@@ -0,0 +1,8 @@
+from sqlalchemy import (
+    create_engine, Column, Float, ForeignKey, Integer, String
+)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# executing instructions from the chinook database
+db = create_engine("postgresql:///chinook")
