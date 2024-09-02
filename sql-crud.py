719f710f333068fca38dd1dc6e494edf7da0cfb1
Michael Brown <ironmonkeydaveb@yahoo.co.uk>
2024-09-02 09:08:57 +0000

    format sql-crud.py

--- sql-crud.py
+++ sql-crud.py
@@ -30,6 +30,7 @@ session = Session()
 # creating the database using declarative_base subclass
 base.metadata.create_all(db)
 
+
 # creating records on our Progammer table
 ada_lovelace = Programmer(
     first_name="Ada",
@@ -82,37 +83,63 @@ tim_berners_lee = Programmer(
 dave_brown = Programmer(
     first_name="Dave",
     last_name="Brown",
-    gender="M",
+    gender="Male",
     nationality="British",
     famous_for="Hacking the planet"
 )
 
-# add each of our programmers to the session
-session.add(ada_lovelace)
-session.add(alan_turing)
-session.add(grace_hopper)
-session.add(margaret_hamilton)
-session.add(bill_gates)
-session.add(tim_berners_lee)
-session.add(dave_brown)
+# add each instance of our programmers to our session
+# session.add(ada_lovelace)
+# session.add(alan_turing)
+# session.add(grace_hopper)
+# session.add(margaret_hamilton)
+# session.add(bill_gates)
+# session.add(tim_berners_lee)
+# session.add(dave_brown)
 
-# update a single record
+# updating a single record
 # programmer = session.query(Programmer).filter_by(id=7).first()
 # programmer.famous_for = "Zero day exploits"
 
-# commit session to the database
+# commit our session to the database
 # session.commit()
 
-# update multiple records
-people = session.query(Programmer)
-for person in people:
-        if person.gender == "F":
-            person.gender = "Female"
-        elif person.gender == "M":
-            person.gender = "Male"
-        else:
-            print("Gender not defined")
+
+# updating multiple records
+# people = session.query(Programmer)
+# for person in people:
+#     if person.gender == "F":
+#         person.gender = "Female"
+#     elif person.gender == "M":
+#         person.gender = "Male"
+#     else:
+#         print("Gender not defined")
+#     session.commit()
+
+
+# delete single record
+fname = input("Enter a first name: ")
+lname = input("Enter a last name: ")
+programmer = session.query(Programmer).filter_by(first_name=fname, last_name=lname).first()
+
+# defensive programming
+if programmer is not None:
+    print("Programmer Found: ", programmer.first_name + " " + programmer.last_name)
+    confirmation = input("Are you sure you want to delete this record? (y/n) ")
+    if confirmation.lower() == "y":
+        session.delete(programmer)
         session.commit()
+        print("Programmer has been deleted")
+    else:
+        print("Programmer not deleted")
+else:
+    print("No records found")
+
+# delete multiple records
+# programmers = session.query(Programmer)
+# for programmer in programmers:
+#     session.delete(programmer)
+#     session.commit()
 
 # query database to find all Programmers
 programmers = session.query(Programmer)
